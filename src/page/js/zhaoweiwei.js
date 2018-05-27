8ff175fce35009869b1450fdc131f008e630d109
liubingyang <252700620@qq.com>
2018-05-27 21:44:35 +0800

    完成设计师部分

---
+++ src/page/js/zhaoweiwei.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper-container.banner-zhaoweiwei', {
+	direction: 'horizontal',
+	loop: true,
+	// 如果需要前进后退按钮
+	navigation: {
+		nextEl: '.next-banner',
+		prevEl: '.prev-banner',
+	},
+
+})
