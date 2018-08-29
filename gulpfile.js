cb9cc4d77a57fd79b8e5fa8c2a4abb9048efb564
liubingyang <252700620@qq.com>
2018-08-29 18:21:11 +0800

    增加gulp打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 // 获取 gulp
 var gulp = require('gulp');
- 
-var imageMin = require('gulp-imagemin');
- 
-gulp.task('image',function(){
-    gulp.src('src/image/*.*')
-        .pipe(imageMin({progressive: true}))
-        .pipe(gulp.dest('src/image2'))
-})
+
+var babel = require("gulp-babel");
+
+
+gulp.task("js", function () {
+  return gulp.src("src/**/*.js")// ES6 源码存放的路径
+    .pipe(babel()) 
+    .pipe(gulp.dest("dist")); //转换成 ES5 存放的路径
+});
+gulp.task('copy',  function() {
+  return gulp.src('src/**/*')
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['copy', 'js']);
